7fc83a3a346a9770b70410c7c5ca20e2f52972e3
klutzy <klutzytheklutzy@gmail.com>
2013-08-31 13:59:25 +0900

    Parse hangul

---
+++ aheui.rs
@@ -0,0 +1,95 @@
+#[deriving(Eq)]
+pub enum Cho {
+    cㄱ, cㄲ, cㄴ, cㄷ, cㄸ, cㄹ, cㅁ, cㅂ,
+    cㅃ, cㅅ, cㅆ, cㅇ, cㅈ, cㅉ, cㅊ, cㅋ,
+    cㅌ, cㅍ, cㅎ, cNone
+}
+
+#[deriving(Eq)]
+pub enum Jung {
+    ㅏ, ㅐ, ㅑ, ㅒ, ㅓ, ㅔ, ㅕ, ㅖ,
+    ㅗ, ㅘ, ㅙ, ㅚ, ㅛ, ㅜ, ㅝ, ㅞ,
+    ㅟ, ㅠ, ㅡ, ㅢ, ㅣ, juNone
+}
+
+#[deriving(Eq)]
+pub enum Jong {
+    joNone,
+    jㄱ, jㄲ, jㄳ, jㄴ, jㄵ, jㄶ, jㄷ, jㄹ,
+    jㄺ, jㄻ, jㄼ, jㄽ, jㄾ, jㄿ, jㅀ, jㅁ,
+    jㅂ, jㅄ, jㅅ, jㅆ, jㅇ, jㅈ, jㅊ, jㅋ,
+    jㅌ, jㅍ, jㅎ
+}
+
+#[deriving(Eq)]
+struct Hangul {
+    cho: Cho,
+    jung: Jung,
+    jong: Jong,
+}
+
+impl Hangul {
+    fn none() -> Hangul {
+        Hangul {
+            cho: cNone,
+            jung: juNone,
+            jong: joNone,
+        }
+    }
+
+    fn from_char(c: char) -> Hangul {
+        let c = c as uint;
+        let ga = '가' as uint;
+        if (c < ga) {
+            return Hangul::none();
+        }
+        let c = c - ga;
+        let cho = c / 28 / 21;
+        let jung = (c / 28) % 21;
+        let jong = c % 28;
+        unsafe {
+            Hangul {
+                cho: std::cast::transmute(cho),
+                jung: std::cast::transmute(jung),
+                jong: std::cast::transmute(jong),
+            }
+        }
+    }
+}
+
+fn main() {
+    let hello_world = [
+        "밤밣따빠밣밟따뿌",
+        "빠맣파빨받밤뚜뭏",
+        "돋밬탕빠맣붏두붇",
+        "볻뫃박발뚷투뭏붖",
+        "뫃도뫃희멓뭏뭏붘",
+        "뫃봌토범더벌뿌뚜",
+        "뽑뽀멓멓더벓뻐뚠",
+        "뽀덩벐멓뻐덕더벅",
+    ];
+    let hello_world = do hello_world.map |&x| {
+        x.iter().map(Hangul::from_char).collect::<~[Hangul]>()
+    };
+    println(fmt!("%?", hello_world));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_hangul() {
+        let 가 = Hangul { cho: cㄱ, jung: ㅏ, jong: joNone };
+        assert!(Hangul::from_char('가') == 가);
+
+        let 아 = Hangul { cho: cㅇ, jung: ㅏ, jong: joNone };
+        assert!(Hangul::from_char('아') == 아);
+
+        let 힣 = Hangul { cho: cㅎ, jung: ㅣ, jong: jㅎ };
+        assert!(Hangul::from_char('힣') == 힣);
+
+        assert!(Hangul::from_char('A') == Hangul::none());
+        assert!(Hangul::from_char('☆') == Hangul::none());
+    }
+}
